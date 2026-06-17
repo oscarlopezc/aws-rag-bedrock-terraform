b26e1165c013b8f7a047946f1eabc212e74e409a
Oscar Lopez <oscarlopezc6090@gmail.com>
2026-06-17 17:50:38 -0500

    update key pair to v3

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -33,14 +33,14 @@ resource "aws_instance" "docker_host" {
 
   subnet_id = aws_subnet.main.id
 
-  key_name = "sre-devops-key"
+  key_name = "sre-devops-key-v3"
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
- vpc_security_group_ids = [
-  aws_security_group.ec2_sg.id
-]
-associate_public_ip_address = true
+  vpc_security_group_ids = [
+    aws_security_group.ec2_sg.id
+  ]
+  associate_public_ip_address = true
 
   user_data = file("${path.module}/userdata/docker-install.sh")
 
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,24 +1,67 @@
-resource "aws_vpc" "main" {
+###################################
+# VPC
+###################################
 
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "sre-devops-vpc"
   }
-
 }
 
-resource "aws_subnet" "main" {
+###################################
+# INTERNET GATEWAY
+###################################
 
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
-  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "sre-devops-igw"
+  }
+}
 
+###################################
+# SUBNET PÚBLICA
+###################################
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+
+  # hace la subnet pública
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "sre-devops-subnet"
   }
+}
 
+###################################
+# ROUTE TABLE PÚBLICA
+###################################
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "sre-devops-public-rt"
+  }
+}
+
+###################################
+# ASOCIACIÓN SUBNET → ROUTE TABLE
+###################################
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.public_rt.id
 }
 

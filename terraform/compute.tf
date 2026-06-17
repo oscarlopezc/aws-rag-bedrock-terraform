d5f194fe8a91bb8c98b28305f844efaa7826ffe4
Oscar Lopez <oscarlopezc6090@gmail.com>
2026-06-17 16:23:01 -0500

    test pipeline

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -37,9 +37,9 @@ resource "aws_instance" "docker_host" {
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
-  security_groups = [
-    aws_security_group.ec2_sg.name
-  ]
+ vpc_security_group_ids = [
+  aws_security_group.ec2_sg.id
+]
 
   user_data = file("${path.module}/userdata/docker-install.sh")
 

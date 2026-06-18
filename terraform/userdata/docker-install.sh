e159f4787e2cc597be969a93369beac67290f234
Oscar Lopez <oscarlopezc6090@gmail.com>
2026-06-18 13:27:21 -0500

    Fix GitHub Actions deployment workflow

--- terraform/userdata/docker-install.sh
+++ terraform/userdata/docker-install.sh
@@ -2,11 +2,19 @@
 
 apt-get update -y
 
+# Instalar Docker
 apt-get install -y docker.io
 
-systemctl start docker
+# Instalar Docker Compose
+apt-get install -y docker-compose
 
+# Instalar AWS CLI
+apt-get install -y awscli
+
+# Iniciar Docker
+systemctl start docker
 systemctl enable docker
 
+# Permitir que ubuntu use Docker sin sudo
 usermod -aG docker ubuntu
 
